f641a9fc8a027d03a2c45fed0fef14257281e6fc
KorMIska <127426030+KorMIska@users.noreply.github.com>
2024-03-06 12:52:53 +0300

    nnw

--- New Apel/Assets/Script/CharacterControl/Atak.cs
+++ New Apel/Assets/Script/CharacterControl/Atak.cs
@@ -7,9 +7,12 @@ public class Atak : MonoBehaviour
 {
     public GameObject object_toSpawn;
     private Animator animator;
-    public Transform spawnPoint;
+    public Transform spawnPoint1;
+    public Transform spawnPoint2;
     public float force = 100;
     public int timer = 200;
+    public int Number—harges = 6;
+    public int CurentCharges = 0;
 
 
 
@@ -22,16 +25,38 @@ public class Atak : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (CurentCharges == 0)
+        {
+            animator.SetTrigger("Recharge");
+        }
+
+        if (Input.GetButtonDown("Fire1") && CurentCharges != 0)
         {
             animator.SetTrigger("Atak");
+            
         }
+
+        
+    }
+
+    void Recharge()
+    {
+        CurentCharges = Number—harges;
     }
 
     void InitOb()
     {
-        var i = Instantiate(object_toSpawn, spawnPoint.position, spawnPoint.rotation);
-        var rb = i.GetComponent<Rigidbody>();
+        int i = animator.GetInteger("NomerAtak");
+        i = i == 0 ? 1 : 0;
+        animator.SetInteger("NomerAtak", i);
+        CurentCharges--;
+        GameObject obg;
+        if (i == 0)
+             obg = Instantiate(object_toSpawn, spawnPoint1.position, spawnPoint1.rotation);
+        else
+             obg = Instantiate(object_toSpawn, spawnPoint2.position, spawnPoint2.rotation);
+
+        var rb = obg.GetComponent<Rigidbody>();
         rb.AddForce(transform.forward * force);
     }
 
